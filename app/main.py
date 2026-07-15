dbd750afefce6300b7433e5603cdf8ab32742065
kiraly <kiraly@thor>
2026-07-15 14:30:42 +0200

    Added same file exclusion condition

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ def copy_file(command: str) -> None:
     cmd = arguments[0]
     src = arguments[1]
     dst = arguments[2]
-    if not cmd == "cp":
+    if not cmd == "cp" or src == dst:
         return
     try:
         with open(src, "rb") as source, open(dst, "wb") as destination:
