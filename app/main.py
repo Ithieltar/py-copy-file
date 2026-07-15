f3f986490d4aaf7605e4a9785c5e36cec2ceb824
kiraly <kiraly@thor>
2026-07-15 14:23:56 +0200

    Implement copy_file function

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def copy_file(command: str) -> None:
+    arguments = command.split()
+    if len(arguments) != 3:
+        print("Incorrect number of arguments.")
+        return
+    cmd = arguments[0]
+    src = arguments[1]
+    dst = arguments[2]
+    if not cmd == "cp":
+        return
+    try:
+        with open(src, "rb") as source, open(dst, "wb") as destination:
+            destination.write(source.read())
+    except FileNotFoundError as e:
+        print(e)
